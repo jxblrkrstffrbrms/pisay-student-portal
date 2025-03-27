3cf7c1eaa76e4cc8b6e4881fdec2e58afdd35835
jxblrkrstffrbrms <jaxblairekristofferramos@gmail.com>
2025-03-27 13:35:40 +0800

    second commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { IonHeader, IonFab, IonDatetime, IonFabButton, IonFabList, IonToolbar, IonIcon, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle,IonCardSubtitle, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonDatetime, IonFab, IonFabButton, IonIcon, IonFabList, IonTitle, IonContent, IonCard, IonCardContent, IonCardTitle,IonCardSubtitle, ExploreContainerComponent]
 })
 export class Tab2Page {
 
